85cf6f1a64e5401d5aff8746f1de48b19a4f02f2
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-13 18:06:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ def self.alphabetical
 def self.new_from_filename(song_string)
 data = song_string.split("-")
 artist_name = data[0]
-song_name = data[1].delete_suffix("/.\/")
+song_name = data[1].delete_suffix("/\.\w+/")
 end  
 binding.pry  
  
