d79467de758151f476a0ae28f2a19e65a03fa288
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-13 17:39:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,13 +38,10 @@ class Song
   end 
   
 def self.find_or_create_by_name(search)
- if @@all.include?(search) 
- self.find_by_name(search)
- else 
- self.create_by_name(search)
+self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
 
- end 
- end
+ 
  end  
  
   
